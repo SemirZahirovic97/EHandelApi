506a9d710360aba4152620d6adc5677bc98d9560
SemirZahirovic97 <semir_guzonja@hotmail.se>
2026-06-10 11:27:41 +0200

    feat: configure Program.cs, EF Core and database migrations

--- EHandelApi/Program.cs
+++ EHandelApi/Program.cs
@@ -1,15 +1,22 @@
+using EHandelApi;
+using EHandelApi.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<EHandelContext>(options =>
+    options.UseSqlite("Data Source=ehandel.db"));
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +24,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
